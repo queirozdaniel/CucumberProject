98c520b44b105fc112d2577437d6bc90b2a6156d
queirozdaniel <danielqueirozak77@gmail.com>
2020-09-08 01:08:21 -0300

    Gerando report em html

--- src/test/java/com/danielqueiroz/runners/RunnerTest.java
+++ src/test/java/com/danielqueiroz/runners/RunnerTest.java
@@ -5,10 +5,10 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty","summary"}, 
+@CucumberOptions(plugin = {"pretty", "summary", "html:target/report.html"}, 
 		features = {"src/test/resources/features/alugar_filme.feature"},
 		glue = {"com.danielqueiroz.steps", "com.danielqueiroz.converters"},
 		tags = "")
-public class Runner {
+public class RunnerTest {
 
 }
